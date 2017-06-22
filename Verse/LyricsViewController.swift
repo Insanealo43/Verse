41284ce65e85673e0ffde37ba507df9b7dc6a005
Andrew Lopez-Vass <andrew.lopez.vass@gmail.com>
2017-06-21 20:51:10 -0700

    Tweaked lyrics scene

--- Verse/LyricsViewController.swift
+++ Verse/LyricsViewController.swift
@@ -14,6 +14,15 @@ class LyricsViewController: UIViewController {
   var song: Song!
 
   @IBOutlet
+  private weak var albumImageView: PhotoImageView!
+
+  @IBOutlet
+  private weak var songLabel: SongLabel!
+
+  @IBOutlet
+  private weak var albumLabel: SongLabel!
+
+  @IBOutlet
   private weak var webView: UIWebView!
 
   @IBOutlet
@@ -21,20 +30,27 @@ class LyricsViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    albumImageView.photo = song.artwork
+    songLabel.setName(for: song)
+    albumLabel.text = song.album
     webView.scrollView.showsVerticalScrollIndicator = false
-    Session.current.getLyrics(for: song)
-    .then { [weak self] lyricsURL in
-      self?.loadURL(lyricsURL)
-    }
-    .catch { [weak self] error in
-      self?.showAlert(error: error)
-    }
+    getLyrics()
   }
 
   @IBAction
   func hideTapped(_ sender: Any) {
     dismiss(animated: true, completion: nil)
     view.backgroundColor = .clear
+  }
+
+  private func getLyrics() {
+    Session.current.getLyrics(for: song)
+      .then { [weak self] lyricsURL in
+        self?.loadURL(lyricsURL)
+      }
+      .catch { [weak self] error in
+        self?.showAlert(error: error)
+    }
   }
 
   private func loadURL(_ url: URL) {
@@ -47,7 +63,11 @@ class LyricsViewController: UIViewController {
       message: nil,
       preferredStyle: .alert
     )
-    alert.addAction(UIAlertAction(title: "OK", style: .default))
+    alert.addAction(
+      UIAlertAction(title: "Dismiss", style: .default) { _ in
+        self.dismiss(animated: true, completion: nil)
+      }
+    )
     present(alert, animated: true, completion: nil)
   }
 
